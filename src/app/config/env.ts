e6aea32f2d030622c6a1641c08acce0a792a6e61
Md Nishan Chowdhury <mdnishanchowdhury.cse@gmail.com>
2026-03-20 19:07:18 +0600

    add database schemas

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -3,14 +3,26 @@ dotenv.config();
 
 interface EnvConfig {
     PORT: string;
-    DATABASE_URL:string;
+    DATABASE_URL: string;
+    BETTER_AUTH_SECRET: string;
+    BETTER_AUTH_URL: string;
+    ACCESS_TOKEN_SECRET: string;
+    REFRESH_TOKEN_SECRET: string;
+    ACCESS_TOKEN_EXPIRES_IN: string;
+    REFRESH_TOKEN_EXPIRES_IN: string;
 }
 
 const loadEnvVariables = (): EnvConfig => {
 
     const requireEnvVariable = [
         'PORT',
-        'DATABASE_URL'
+        'DATABASE_URL',
+        'BETTER_AUTH_SECRET',
+        'BETTER_AUTH_URL',
+        'ACCESS_TOKEN_SECRET',
+        'REFRESH_TOKEN_SECRET',
+        'ACCESS_TOKEN_EXPIRES_IN',
+        'REFRESH_TOKEN_EXPIRES_IN',
     ]
 
     requireEnvVariable.forEach((varible) => {
@@ -21,6 +33,12 @@ const loadEnvVariables = (): EnvConfig => {
     return {
         PORT: process.env.PORT as string,
         DATABASE_URL: process.env.DATABASE_URL as string,
+        BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
+        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
+        ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+        REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
+        ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+        REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
     }
 }
 export const envVars = loadEnvVariables();
